d84f40c0677746515fbbfebed59a0b94850bada7
Douglas Americo <douglas.alex.1212@gmail.com>
2026-03-16 21:09:10 -0300

    Primeira aula de calculo

---
+++ aula3.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+
+let nome = prompt("Por favor digite seu nome: ");
+console.log("O nome digitado foi " + nome);
---
+++ calculode2.js
@@ -0,0 +1,5 @@
+console.log(Math.pow(5,2));
+
+console.log(Math.sqrt(9));
+
+

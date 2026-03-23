632aba6e7c986513ac1813bb0b17c0c31ad0638a
Douglas Americo <douglas.alex.1212@gmail.com>
2026-03-23 20:17:19 -0300

    subindo a formula do exercicio

---
+++ Aula4.js
@@ -0,0 +1,11 @@
+let primeiroElemento = Math.pow(5,3);
+let segundoElemento = 3;
+let terceiroElemento = Math.pow(4,2);
+
+console.log("Primeiro Elemento "+primeiroElemento);
+console.log("Segundo Elemento "+segundoElemento);
+console.log("Terceiro Elemento "+terceiroElemento);
+
+console.log("5³ - 3 x 4² = "+(primeiroElemento - segundoElemento * terceiroElemento));
+
+
